8ebe522d53cd99c5cdd7a7f61df17c3c8642695c
efierr <erickfierro0413@gmail.com>
2024-05-09 14:13:22 -0400

    finishedstaticheader

---
+++ src/components/Search.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Search.css"
+export default function Search() {
+  return (
+    <div className="search_bar">
+      <input type="text" id="search" placeholder="search videos" />
+    </div>
+  );
+}
