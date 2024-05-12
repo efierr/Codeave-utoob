79d5e34edc2c02451b7c460eab23ed377590597a
AntCannon <anthonycannonier@gmail.com>
2024-05-12 09:55:56 -0400

    add page layout

---
+++ src/components/Video.jsx
@@ -0,0 +1,15 @@
+import "./Video.css"
+export default function Video() {
+  
+  return (
+    <section className="video-view">
+      <div className="video">
+
+      </div>
+      <div className="comment-section">
+        <h3>Comment Form</h3>
+        <h3>Comments</h3>
+      </div>
+    </section>
+  )
+}
