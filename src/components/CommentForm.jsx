2fedb8abf4dadf32b9a07fdac4d74d843184fb0f
AntCannon <anthonycannonier@gmail.com>
2024-05-12 13:28:12 -0400

    add register, devtool, and control to form

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -1,27 +1,37 @@
 import "./CommentForm.css"
 import { useForm } from "react-hook-form"
+import { DevTool } from "@hookform/devtools"
 
 export default function CommentForm() {
   const commentForm = useForm();
 
-  const { register } = commentForm
+  const { register, control } = commentForm
 
   return (
-    <form>
-      <label>Name
-        <br />
-        <input type="text" id="username"
-        name="username" />
-      </label>
+    <>
+      <form>
+        <label>Name
+          <br />
+          <input
+            type="text"
+            id="username"
+            {...register("username")}
+            />
+        </label>
 
-      <label>Comment
-        <br />
-        <input type="text" id="comment"
-        name="comment" />
-      </label>
+        <label>Comment
+          <br />
+          <input
+            type="text"
+            id="comment"
+            {...register("comment")}
+            />
+        </label>
 
-      <button>Submit</button>
+        <button>Submit</button>
 
-    </form>
+      </form>
+      <DevTool control={control} />
+    </>
   )
 }
