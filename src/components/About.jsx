621cf7162097cf012e85c856e2805c70afcf455e
Venkata Raji Reddy Eda <evrajireddy@gmail.com>
2024-05-13 20:36:49 -0400

    style css About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,31 +11,27 @@ export default function About() {
             <img src="https://ca.slack-edge.com/TCVA3PF24-U063SF1VCJ3-f07910bf77c9-512" alt="Anthony" className="Avatar"  />
             <p>Name: Anthony C.</p>
             <p>Title: Sr.FullStack Developer </p>
-            <p>JobDescription: Establishing optimal site functionality using both Front-end and Back-end Web Development Domains. Ensuring site compatibility across a range of screen resolutions.Working with a team of other developers to fix bugs in the code, implement new functions, and make changes to existing ones. Essentially,responsible for fully-fledged platforms using a range of different technologies.</p> 
+            <p>JobDescription: Establishing optimal site functionality using both Front-end  
+            and Back-end Web Development Domains. Ensuring site compatibility across a range of screen resolutions.Working with a team of other developers to fix bugs in the code, implement new functions, and make changes to existing ones. Essentially,responsible for fully-fledged platforms using a range of different technologies.</p> 
             </div>
 
             <div className="developer">
             <img src="https://ca.slack-edge.com/TCVA3PF24-U06471DTBDY-08b366aced14-512" alt="Erick" className="Avatar" />
             <p>Name: Erick</p>
             <p>Title: Jr.FullStack Developer</p>
-            <p>JobDescription:</p>
+            <p>JobDescription: Development and maintenance of both front-end and back-end systems of a website or application. Proficient in both client-side and server-side technologies, capable of handling the entire development process of a project</p>
             </div>
 
             <div className="developer">
             <img src="https://ca.slack-edge.com/TCVA3PF24-U06471JD41G-608e59742117-512" alt="Venkata" className="Avatar" />
             <p>Name: Venkata</p>
             <p>Title: Jr.FullStack Developer</p>
-            <p>JobDescription:</p>
+            <p>JobDescription: Development and maintenance of both front-end and back-end systems of a website or application. Proficient in both client-side and server-side technologies, capable of handling the entire development process of a project</p>
             </div>
 
-
-
-
-            </div>
-
+        </div>
         
-
-    )
+        )
 }
 
 
