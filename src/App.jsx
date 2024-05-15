2d0a28aca385bc5a75ed49989cfe692c9f1533f3
efierr <erickfierro0413@gmail.com>
2024-05-14 22:04:32 -0400

    handle search fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,57 @@
-import "./App.css";
+import "./App.css";``
 import { useState, useEffect } from "react";
-import { Routes, Route } from "react-router-dom"
-import { getMostPlayed } from "./utils/youtubeFetch.js"
-import videoDB from "./utils/videoDB.json"
+import { Routes, Route } from "react-router-dom";
+import { getMostPlayed } from "./utils/youtubeFetch.js";
+import videoDB from "./utils/videoDB.json";
 import Nav from "./components/Nav.jsx";
 import Search from "./components/Search.jsx";
-import Home from "./components/Home.jsx"
-import VideoIndex from "./components/VideoIndex.jsx"
-import VideoView from "./components/VideoView.jsx"
-import About from "./components/About.jsx"
+import Home from "./components/Home.jsx";
+import VideoIndex from "./components/VideoIndex.jsx";
+import VideoView from "./components/VideoView.jsx";
+import About from "./components/About.jsx";
 import Footer from "./components/Footer.jsx";
 
 export default function App() {
-  const [ videos, setVideos ] = useState([])
-  const [ videoMap, setVideoMap ] = useState({})
+  const [videos, setVideos] = useState([]);
+  const [videoMap, setVideoMap] = useState({});
+  const [searchResults, setSearchResults] = useState([])
+  const [userSearched, setUserSearched] = useState(false)
 
   useEffect(() => {
     if (!videos.length) {
       getMostPlayed()
-      .then(data => {
-        setVideos(data.items)
-        setVideoMap(indexVideos(data.items))
-      })
-      .catch(err => console.error(err))
+        .then((data) => {
+          setVideos(data.items);
+          setVideoMap(indexVideos(data.items));
+        })
+        .catch((err) => console.error(err));
     }
-  }, [])
+  }, []);
 
   return (
     <>
-    <header>
-      <Nav/>
-      <Search/>
-    </header>
-    <main> 
-       <Routes>
-        <Route path="/"
-          element={<VideoIndex
-            videos={videos} />}
-        />
-        <Route path="/videos/:videoID"
-          element={<VideoView
-            videos={videos}
-            videoMap={videoMap}
-          />}
-        />
-        <Route path="/about"
-          element={<About />}
-        />
-      </Routes>
-    </main>
-    <Footer />
+      <header>
+        <Nav />
+        <Search setSearchResults={setSearchResults}/>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<VideoIndex videos={videos} />} />
+          <Route
+            path="/videos/:videoID"
+            element={<VideoView videos={videos} videoMap={videoMap} />}
+          />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+      <Footer />
     </>
   );
 }
 
 // helper - index videos
 function indexVideos(arr) {
-  const indexedVideos = {}
-  arr.forEach(({id}, idx) => indexedVideos[id] = idx)
-  return indexedVideos
+  const indexedVideos = {};
+  arr.forEach(({ id }, idx) => (indexedVideos[id] = idx));
+  return indexedVideos;
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,36 @@
-import React from "react";
-import "./Search.css"
-export default function Search() {
+import React, { useState } from "react";
+import "./Search.css";
+import { searchForVideos } from "../utils/youtubeFetch.js";
+
+export default function Search({ setSearchResults }) {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  function handleChange(event) {
+    setSearchQuery(event.target.value);
+  }
+
+  function handleSearch() {
+    // search query
+    // fetch for videos based off search q
+    console.log(searchQuery);
+    searchForVideos(searchQuery)
+      .then((response) => {
+        console.log(response);
+        setSearchResults(response);
+      })
+      .catch((error) => console.error(error));
+  }
+
   return (
     <div className="search_bar">
-      <input type="text" id="search" placeholder="search videos" />
+      <input
+        type="text"
+        id="search"
+        placeholder="search videos"
+        value={searchQuery}
+        onChange={handleChange}
+      />
+      <button onClick={handleSearch}>search</button>
     </div>
   );
 }
